8880d60e9485c611aac5745169934d99c181f6e6
mollyincali <mollyehardy@gmail.com>
2020-08-26 08:00:35 -0700

    updating read.me

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ![title](images/title.jpg)
 
-![title](images/instacartlogo.png)
 - Instacart partners with most supermarkets, and some local speciality shops depending on your area. Once you place an order a "personal shopper" will fulfill and deliver your order to your door on the same day
 - This dataset is made up of 3 million food orders from about 120,000 customers from the company spanned across multiple CSV files
 
@@ -9,6 +8,12 @@
 ![title](images/dayofweek.png)
 ![title](images/hour.png)
 
+## Question Number One:
+- Can I predict if an item will be reordered by a customer?
+
+## Question Number Two:
+- Can I group users together based on their purchases and recommend new items?
+
 ## Supervised Machine Learning
 - Built a Decision Tree with mean accuracy score of <num>
 - Ran gridsearch to find best parameters for a Decision Tree Classifier <num>
@@ -16,7 +21,7 @@
 - Ran gridsearch to find best parameters for a Random Forest Classifier <num>
 
 ## Unsupervised Machine Learning
-- Latent Features:
+- PCA: 
 
 
 #### Credits
--- notebooks/graphing.py
+++ notebooks/graphing.py
@@ -4,15 +4,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set()
 
-def make_graph(df, col_x, col_y, title, x_label, y_label)
+def make_graph(df, col_x, col_y, title, x_label, y_label):
     fig, ax = plt.subplots(figsize = (20, 10))
     ax = sns.barplot(df[col_x], df[col_y], palette = citrus)
-    ax.set_ylabel(ylabel, fontdict=fontaxis)
-    ax.set_xlabel(xlabel, fontdict=fontaxis)
-    plt.title(title fontdict=fonttitle)
+    ax.set_ylabel(y_label, fontdict=fontaxis)
+    ax.set_xlabel(x_label, fontdict=fontaxis)
+    ax.tick_params(axis='both', which='major', labelsize=18)
+    plt.title(title, fontdict=fonttitle)
     plt.show()
-
-
+    make_graph(hours, 'index', 'order_hour_of_day', 'Orders by Hour', "Hours", 'Number of Orders')
 if __name__ == "__main__":
     full = pd.read_csv("../full.csv")
 
@@ -25,24 +25,24 @@ if __name__ == "__main__":
     citrus = [color1, color2, color3, color4, color5]
     sns.palplot(sns.color_palette(citrus))
 
-    fonttitle = {'fontname':'Helvetica', 'fontsize':25}
-    fontaxis = {'fontname':'Helvetica', 'fontsize':18}
+    fonttitle = {'fontname':'Helvetica', 'fontsize':30}
+    fontaxis = {'fontname':'Helvetica', 'fontsize':20}
 
     #---    things to graph
     high_reord = full.groupby('product_name').agg({"reordered":"count"})\
                      .sort_values(by='reordered', ascending = False).reset_index()[:50]
-    dow = full['order_dow'].value_counts().reset_index()
+    # dow = full['order_dow'].value_counts().reset_index()
     hours = full['order_hour_of_day'].value_counts().reset_index().sort_values(by="index")
-    num_order = full['order_number'].value_counts()
-    top_depart = full.groupby('department').agg({"product_name":"size"})\
-                     .sort_values(by='product_name', ascending = False).reset_index()
-    top_aisle = full.groupby('aisle').agg({"product_name":"size"})\
-                     .sort_values(by='product_name', ascending = False).reset_index()[:25]
-    bottom_aisle = full.groupby('aisle').agg({"product_name":"size"})\
-                     .sort_values(by='product_name', ascending = False).reset_index()[84:]
+    # num_order = full['order_number'].value_counts()
+    # top_depart = full.groupby('department').agg({"product_name":"size"})\
+    #                  .sort_values(by='product_name', ascending = False).reset_index()
+    # top_aisle = full.groupby('aisle').agg({"product_name":"size"})\
+    #                  .sort_values(by='product_name', ascending = False).reset_index()[:25]
+    # bottom_aisle = full.groupby('aisle').agg({"product_name":"size"})\
+    #                  .sort_values(by='product_name', ascending = False).reset_index()[84:]
 
     #---    plots
-    make_graph(hours, 'index', 'order_hour_of_day', 'Orders by Hour',"Hours", 'Number of Orders')
-    make_graph(dow, 'index', 'order_dow', 'Orders by Day of Week', "Day", 'Number of Orders', )
-    make_graph(high_reord, 'product_name', 'reordered', 'Highest Reordered Item', "Item", 'Number of Reorders')
+    make_graph(hours, 'index', 'order_hour_of_day', 'Orders by Hour', "Hours", 'Number of Orders')
+    # make_graph(dow, 'index', 'order_dow', 'Orders by Day of Week', "Day", 'Number of Orders', )
+    # make_graph(high_reord, 'product_name', 'reordered', 'Highest Reordered Item', "Item", 'Number of Reorders')
                           
